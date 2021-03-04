a6e9d414928dcaf42934dc52f73bc126496b283c
whoisjones <jonas.golde@gmail.com>
2021-03-04 18:43:35 +0100

    bert training

--- TREC6_TO_TREC50_ENTAILMENT_BASE_TRAIN.py
+++ TREC6_TO_TREC50_ENTAILMENT_BASE_TRAIN.py
@@ -39,62 +39,60 @@ def train_few_shot_model(path):
 
     for no_examples in number_of_seen_examples:
         for run_number in range(5):
-            base_pretrained_tars = TARSClassifier.load(base_pretrained_model_path)
-
-            trec50_label_name_map = {'ENTY:sport': 'question about entity sport',
-                                     'ENTY:dismed': 'question about entity diseases medicine',
-                                     'LOC:city': 'question about location city',
-                                     'DESC:reason': 'question about description reasons',
-                                     'NUM:other': 'question about number other',
-                                     'LOC:state': 'question about location state',
-                                     'NUM:speed': 'question about number speed',
-                                     'NUM:ord': 'question about number order ranks',
-                                     'ENTY:event': 'question about entity event',
-                                     'ENTY:substance': 'question about entity element substance',
-                                     'NUM:perc': 'question about number percentage fractions',
-                                     'ENTY:product': 'question about entity product',
-                                     'ENTY:animal': 'question about entity animal',
-                                     'DESC:manner': 'question about description manner of action',
-                                     'ENTY:cremat': 'question about entity creative pieces inventions books',
-                                     'ENTY:color': 'question about entity color',
-                                     'ENTY:techmeth': 'question about entity technique method',
-                                     'NUM:dist': 'question about number distance measure',
-                                     'NUM:weight': 'question about number weight',
-                                     'LOC:mount': 'question about location mountains',
-                                     'HUM:title': 'question about person title',
-                                     'HUM:gr': 'question about person group organization of persons',
-                                     'HUM:desc': 'question about person description',
-                                     'ABBR:abb': 'question about abbreviation abbreviation',
-                                     'ENTY:currency': 'question about entity currency',
-                                     'DESC:def': 'question about description definition',
-                                     'NUM:code': 'question about number code',
-                                     'LOC:other': 'question about location other',
-                                     'ENTY:other': 'question about entity other',
-                                     'ENTY:body': 'question about entity body organ',
-                                     'ENTY:instru': 'question about entity musical instrument',
-                                     'ENTY:termeq': 'question about entity term equivalent',
-                                     'NUM:money': 'question about number money prices',
-                                     'NUM:temp': 'question about number temperature',
-                                     'LOC:country': 'question about location country',
-                                     'ABBR:exp': 'question about abbreviation expression',
-                                     'ENTY:symbol': 'question about entity symbol signs',
-                                     'ENTY:religion': 'question about entity religion',
-                                     'HUM:ind': 'question about person individual',
-                                     'ENTY:letter': 'question about entity letters characters',
-                                     'NUM:date': 'question about number date',
-                                     'ENTY:lang': 'question about entity language',
-                                     'ENTY:veh': 'question about entity vehicle',
-                                     'NUM:count': 'question about number count',
-                                     'ENTY:word': 'question about entity word special property',
-                                     'NUM:period': 'question about number period lasting time',
-                                     'ENTY:plant': 'question about entity plant',
-                                     'ENTY:food': 'question about entity food',
-                                     'NUM:volsize': 'question about number volume size',
-                                     'DESC:desc': 'question about description description'
-                                     }
-            whole_corpus: Corpus = TREC_50(label_name_map=trec50_label_name_map)
-
             if no_examples == 0 and run_number == 0:
+                base_pretrained_tars = TARSClassifier.load(base_pretrained_model_path)
+                trec50_label_name_map = {'ENTY:sport': 'question about entity sport',
+                                         'ENTY:dismed': 'question about entity diseases medicine',
+                                         'LOC:city': 'question about location city',
+                                         'DESC:reason': 'question about description reasons',
+                                         'NUM:other': 'question about number other',
+                                         'LOC:state': 'question about location state',
+                                         'NUM:speed': 'question about number speed',
+                                         'NUM:ord': 'question about number order ranks',
+                                         'ENTY:event': 'question about entity event',
+                                         'ENTY:substance': 'question about entity element substance',
+                                         'NUM:perc': 'question about number percentage fractions',
+                                         'ENTY:product': 'question about entity product',
+                                         'ENTY:animal': 'question about entity animal',
+                                         'DESC:manner': 'question about description manner of action',
+                                         'ENTY:cremat': 'question about entity creative pieces inventions books',
+                                         'ENTY:color': 'question about entity color',
+                                         'ENTY:techmeth': 'question about entity technique method',
+                                         'NUM:dist': 'question about number distance measure',
+                                         'NUM:weight': 'question about number weight',
+                                         'LOC:mount': 'question about location mountains',
+                                         'HUM:title': 'question about person title',
+                                         'HUM:gr': 'question about person group organization of persons',
+                                         'HUM:desc': 'question about person description',
+                                         'ABBR:abb': 'question about abbreviation abbreviation',
+                                         'ENTY:currency': 'question about entity currency',
+                                         'DESC:def': 'question about description definition',
+                                         'NUM:code': 'question about number code',
+                                         'LOC:other': 'question about location other',
+                                         'ENTY:other': 'question about entity other',
+                                         'ENTY:body': 'question about entity body organ',
+                                         'ENTY:instru': 'question about entity musical instrument',
+                                         'ENTY:termeq': 'question about entity term equivalent',
+                                         'NUM:money': 'question about number money prices',
+                                         'NUM:temp': 'question about number temperature',
+                                         'LOC:country': 'question about location country',
+                                         'ABBR:exp': 'question about abbreviation expression',
+                                         'ENTY:symbol': 'question about entity symbol signs',
+                                         'ENTY:religion': 'question about entity religion',
+                                         'HUM:ind': 'question about person individual',
+                                         'ENTY:letter': 'question about entity letters characters',
+                                         'NUM:date': 'question about number date',
+                                         'ENTY:lang': 'question about entity language',
+                                         'ENTY:veh': 'question about entity vehicle',
+                                         'NUM:count': 'question about number count',
+                                         'ENTY:word': 'question about entity word special property',
+                                         'NUM:period': 'question about number period lasting time',
+                                         'ENTY:plant': 'question about entity plant',
+                                         'ENTY:food': 'question about entity food',
+                                         'NUM:volsize': 'question about number volume size',
+                                         'DESC:desc': 'question about description description'
+                                         }
+                whole_corpus: Corpus = TREC_50(label_name_map=trec50_label_name_map)
                 tp = 0
                 all = 0
                 classes = [key for key in trec50_label_name_map.values()]
@@ -113,6 +111,60 @@ def train_few_shot_model(path):
                     file.write(f"Total labels: {all}")
 
             elif no_examples > 0:
+                base_pretrained_tars = TARSClassifier.load(base_pretrained_model_path)
+                trec50_label_name_map = {'ENTY:sport': 'question about entity sport',
+                                         'ENTY:dismed': 'question about entity diseases medicine',
+                                         'LOC:city': 'question about location city',
+                                         'DESC:reason': 'question about description reasons',
+                                         'NUM:other': 'question about number other',
+                                         'LOC:state': 'question about location state',
+                                         'NUM:speed': 'question about number speed',
+                                         'NUM:ord': 'question about number order ranks',
+                                         'ENTY:event': 'question about entity event',
+                                         'ENTY:substance': 'question about entity element substance',
+                                         'NUM:perc': 'question about number percentage fractions',
+                                         'ENTY:product': 'question about entity product',
+                                         'ENTY:animal': 'question about entity animal',
+                                         'DESC:manner': 'question about description manner of action',
+                                         'ENTY:cremat': 'question about entity creative pieces inventions books',
+                                         'ENTY:color': 'question about entity color',
+                                         'ENTY:techmeth': 'question about entity technique method',
+                                         'NUM:dist': 'question about number distance measure',
+                                         'NUM:weight': 'question about number weight',
+                                         'LOC:mount': 'question about location mountains',
+                                         'HUM:title': 'question about person title',
+                                         'HUM:gr': 'question about person group organization of persons',
+                                         'HUM:desc': 'question about person description',
+                                         'ABBR:abb': 'question about abbreviation abbreviation',
+                                         'ENTY:currency': 'question about entity currency',
+                                         'DESC:def': 'question about description definition',
+                                         'NUM:code': 'question about number code',
+                                         'LOC:other': 'question about location other',
+                                         'ENTY:other': 'question about entity other',
+                                         'ENTY:body': 'question about entity body organ',
+                                         'ENTY:instru': 'question about entity musical instrument',
+                                         'ENTY:termeq': 'question about entity term equivalent',
+                                         'NUM:money': 'question about number money prices',
+                                         'NUM:temp': 'question about number temperature',
+                                         'LOC:country': 'question about location country',
+                                         'ABBR:exp': 'question about abbreviation expression',
+                                         'ENTY:symbol': 'question about entity symbol signs',
+                                         'ENTY:religion': 'question about entity religion',
+                                         'HUM:ind': 'question about person individual',
+                                         'ENTY:letter': 'question about entity letters characters',
+                                         'NUM:date': 'question about number date',
+                                         'ENTY:lang': 'question about entity language',
+                                         'ENTY:veh': 'question about entity vehicle',
+                                         'NUM:count': 'question about number count',
+                                         'ENTY:word': 'question about entity word special property',
+                                         'NUM:period': 'question about number period lasting time',
+                                         'ENTY:plant': 'question about entity plant',
+                                         'ENTY:food': 'question about entity food',
+                                         'NUM:volsize': 'question about number volume size',
+                                         'DESC:desc': 'question about description description'
+                                         }
+                whole_corpus: Corpus = TREC_50(label_name_map=trec50_label_name_map)
+                
                 few_shot_corpus = create_few_shot_corpus(no_examples, whole_corpus)
 
                 base_pretrained_tars.add_and_switch_to_new_task("TREC_50", label_dictionary=few_shot_corpus.make_label_dictionary())
@@ -174,5 +226,5 @@ if __name__ == "__main__":
     experiment = "1_entailment_baseline_mnli"
     task = "trec6_to_trec50"
     experiment_path = f"{path}/{experiment}/{task}"
-    #train_base_model(experiment_path, document_embeddings="facebook/bart-large-mnli")
+    train_base_model(experiment_path, document_embeddings="bert-base-uncased")
     train_few_shot_model(experiment_path)
