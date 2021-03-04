cc6561cd48947f6939ea1a00986d0f470af3e4a5
whoisjones <jonas.golde@gmail.com>
2021-03-04 11:14:24 +0100

    first eval

--- TREC6_TO_TREC50_ENTAILMENT_BASE_TRAIN.py
+++ TREC6_TO_TREC50_ENTAILMENT_BASE_TRAIN.py
@@ -1,23 +1,44 @@
-from flair.data import Corpus
-from flair.datasets import TREC_50
-import random
-from flair.models.text_classification_model import TARSClassifier
+import flair
 import os
-import logging
+from flair.data import Corpus
+from flair.datasets import TREC_6, TREC_50, SentenceDataset
+from flair.models.text_classification_model import TARSClassifier
+from flair.trainers import ModelTrainer
+import random
 
-#flair.device = "cuda:0"
+def train_base_model():
+    # 1. define label names in natural language since some datasets come with cryptic set of labels
+    label_name_map = {'ENTY':'question about entity',
+                      'DESC':'question about description',
+                      'ABBR':'question about abbreviation',
+                      'HUM':'question about person',
+                      'NUM':'question about number',
+                      'LOC':'question about location'
+                      }
 
-def main():
+    # 2. get the corpus
+    corpus: Corpus = TREC_6(label_name_map=label_name_map)
+
+    # 3. create a TARS classifier
+    tars = TARSClassifier(task_name='TREC_6', label_dictionary=corpus.make_label_dictionary(), document_embeddings="facebook/bart-large-mnli")
+
+    # 4. initialize the text classifier trainer
+    trainer = ModelTrainer(tars, corpus)
+
+    # 5. start the training
+    trainer.train(base_path='experiments/1_entailment_baseline_mnli/trec6_to_trec50/pretrained_model', # path to store the model artifacts
+                  learning_rate=0.02, # use very small learning rate
+                  mini_batch_size=16,
+                  max_epochs=20, # terminate after 10 epochs
+                  embeddings_storage_mode='gpu')
+
+def train_few_shot_model():
     path = "experiments/1_entailment_baseline_mnli/trec6_to_trec50/"
-    number_of_seen_examples = 0
-    experiment = f"{number_of_seen_examples}_examples"
-    logging_path = os.path.join(path, experiment)
     model = os.path.join(path, "pretrained_model/best-model.pt")
-    logging.basicConfig(filename=f'{logging_path}/result.log', filemode='w',
-                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s')
-
+    number_of_seen_examples = 1
+    
     for run_number in range(5):
-        model = os.path.join(path, "pretrained_model/best-model.pt")
+        tars = TARSClassifier.load(model)
 
         trec50_label_name_map = {'ENTY:sport': 'question about entity sport',
                                  'ENTY:dismed': 'question about entity diseases medicine',
@@ -72,46 +93,60 @@ def main():
                                  }
         corpus: Corpus = TREC_50(label_name_map=trec50_label_name_map)
 
-        tars = TARSClassifier.load(model)
+        train_id_label_tuples = [(x.labels[0].value, id) for id, x in enumerate(corpus.train.dataset.sentences)]
+        train_label_ids_mapping = {}
+        for key, id in train_id_label_tuples:
+            if key not in train_label_ids_mapping:
+                train_label_ids_mapping[key] = [id]
+            else:
+                train_label_ids_mapping[key].append(id)
 
-        tp = 0
-        all = 0
-        classes = [key for key in trec50_label_name_map.values()]
+        train_sentences = []
+        for label, ids in train_label_ids_mapping.items():
+            random.seed(run_number)
+            samples = random.sample(ids, number_of_seen_examples)
+            for sample in samples:
+                train_sentences.append(corpus.train.dataset.sentences[sample])
 
-        for sentence in corpus.train:
-            tars.predict_zero_shot(sentence, classes, multi_label=False)
-            true = sentence.get_labels("class")[0]
-            pred = sentence.get_labels("label")[0]
-            if pred:
-                if pred.value == true.value:
-                    tp += 1
-            all += 1
+        test_id_label_tuples = [(x.labels[0].value, id) for id, x in enumerate(corpus.test.sentences)]
+        test_label_ids_mapping = {}
+        for key, id in test_id_label_tuples:
+            if key not in test_label_ids_mapping:
+                test_label_ids_mapping[key] = [id]
+            else:
+                test_label_ids_mapping[key].append(id)
 
-        for sentence in corpus.test:
-            tars.predict_zero_shot(sentence, classes, multi_label=False)
-            true = sentence.get_labels("class")[0]
-            pred = sentence.get_labels("label")[0]
-            if pred:
-                if pred.value == true.value:
-                    tp += 1
-            all += 1
+        test_sentences = []
+        for label, ids in test_label_ids_mapping.items():
+            random.seed(42)
+            samples = random.sample(ids, 1)
+            for sample in samples:
+                test_sentences.append(corpus.test.sentences[sample])
 
-        for sentence in corpus.dev:
-            tars.predict_zero_shot(sentence, classes, multi_label=False)
-            true = sentence.get_labels("class")[0]
-            pred = sentence.get_labels("label")[0]
-            if pred:
-                if pred.value == true.value:
-                    tp += 1
-            all += 1
+        random.seed(run_number)
+        random.shuffle(train_sentences)
+        # training dataset consisting of four sentences (2 labeled as "food" and 2 labeled as "drink")
+        train = SentenceDataset(
+            train_sentences
+        )
 
-        logging.warning(50*'-')
-        logging.warning(f"Run {run_number}")
-        logging.warning(f"Seen examples from TREC50: {number_of_seen_examples}")
-        logging.warning(f"Accuracy: {tp / all}")
-        logging.warning(f"TP:{tp} out of {all}.")
+        test = SentenceDataset(
+            test_sentences
+        )
 
-    logging.warning(50 * '-')
+        # make a corpus with train and test split
+        few_shot_corpus = Corpus(train=train)
+
+        tars.add_and_switch_to_new_task("TREC_50", label_dictionary=few_shot_corpus.make_label_dictionary())
+
+        trainer = ModelTrainer(tars, few_shot_corpus)
+
+        # 5. start the training
+        trainer.train(base_path=f'experiments/1_entailment_baseline_mnli/trec6_to_trec50/pretrained_model_{number_of_seen_examples}_examples/run_{run_number}', # path to store the model artifacts
+                      learning_rate=0.02, # use very small learning rate
+                      mini_batch_size=16,
+                      max_epochs=20, # terminate after 10 epochs
+                      embeddings_storage_mode='gpu')
 
 if __name__ == "__main__":
-    main()
+    train_few_shot_model()
