3d43af234d3950d1840ad078f703a3ffdf8f2cfc
whoisjones <jonas.golde@gmail.com>
2021-03-05 10:42:41 +0100

    change gpu

--- DBPEDIA_TO_AGNEWS_EXPERIMENT.py
+++ DBPEDIA_TO_AGNEWS_EXPERIMENT.py
@@ -45,6 +45,7 @@ def train_base_model(path, document_embeddings):
     trainer.train(base_path=f"{path}/pretrained_model", # path to store the model artifacts
                   learning_rate=0.02, # use very small learning rate
                   mini_batch_size=16,
+                  mini_batch_chunk_size=4,
                   max_epochs=20)
 
 def train_few_shot_model(path):
@@ -112,6 +113,7 @@ def train_few_shot_model(path):
                 trainer.train(base_path=outpath, # path to store the model artifacts
                               learning_rate=0.02, # use very small learning rate
                               mini_batch_size=16,
+                              mini_batch_chunk_size=4,
                               max_epochs=20)
 
 def create_few_shot_corpus(number_examples, corpus):
