80261a1008bb238e3e76b513888a271ef999ebc7
whoisjones <jonas.golde@gmail.com>
2021-03-06 16:18:50 +0100

    change gpu assignment

--- PRETRAIN_ENTAILMENT.py
+++ PRETRAIN_ENTAILMENT.py
@@ -1,20 +1,81 @@
 from datasets import load_dataset, load_metric
 from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+import numpy as np
+import torch
 
 def main():
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
+
+    torch.device("cuda:1")
+
+    task_to_keys = {
+        "cola": ("sentence", None),
+        "mnli": ("premise", "hypothesis"),
+        "mnli-mm": ("premise", "hypothesis"),
+        "mrpc": ("sentence1", "sentence2"),
+        "qnli": ("question", "sentence"),
+        "qqp": ("question1", "question2"),
+        "rte": ("sentence1", "sentence2"),
+        "sst2": ("sentence", None),
+        "stsb": ("sentence1", "sentence2"),
+        "wnli": ("sentence1", "sentence2"),
+    }
+    model_checkpoint = "bert-base-uncased"
     actual_task = "mnli"
+    sentence1_key, sentence2_key = task_to_keys[actual_task]
+    validation_key = "validation_matched"
+
+    metric_name = "accuracy"
+    num_labels = 2
+
     dataset = load_dataset("glue", actual_task)
     metric = load_metric('glue', actual_task)
-    task_to_keys = {"mnli": ("premise", "hypothesis"),"rte": ("sentence1", "sentence2")}
-    sentence1_key, sentence2_key = task_to_keys[actual_task]
+
+    for split in ["train", "validation_matched", "validation_mismatched", "test_matched", "test_mismatched"]:
+        dataset[split].features["label"].num_classes = 2
+        dataset[split].features["label"].names = ["entailment", "non-entailment"]
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
 
     def preprocess_function(examples):
         if sentence2_key is None:
             return tokenizer(examples[sentence1_key], truncation=True)
+        labels = [label if label in [0,1] else 1 for label in examples["label"]]
+        examples["label"] = labels
         return tokenizer(examples[sentence1_key], examples[sentence2_key], truncation=True)
 
     encoded_dataset = dataset.map(preprocess_function, batched=True)
+
+    args = TrainingArguments(
+        "mnli",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=5,
+        weight_decay=0.01,
+        load_best_model_at_end=True,
+        metric_for_best_model=metric_name,
+    )
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=encoded_dataset["train"],
+        eval_dataset=encoded_dataset[validation_key],
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+
+    trainer.train()
+
+    trainer.evaluate()
 
 
 if __name__ == "__main__":
