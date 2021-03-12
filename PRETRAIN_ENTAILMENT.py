aa8616f70b88157a5caedb15ee1333fd0f676a9c
whoisjones <jonas.golde@gmail.com>
2021-03-12 17:22:32 +0100

    pretrain fever

--- PRETRAIN_ENTAILMENT.py
+++ PRETRAIN_ENTAILMENT.py
@@ -6,7 +6,6 @@ import flair
 import os
 
 def main():
-    """
     model_checkpoint = "bert-base-uncased"
     mnli_dataset = load_dataset("glue", "mnli")
     metric_name = "accuracy"
@@ -23,12 +22,12 @@ def main():
     def preprocess_function(examples):
         labels = [label if label in [0,1] else 1 for label in examples["label"]]
         examples["label"] = labels
-        return tokenizer(examples["hypothesis"], examples["premise"], truncation=True)
+        return tokenizer(examples["premise"], examples["hypothesis"], truncation=True)
 
     encoded_dataset = mnli_dataset.map(preprocess_function, batched=True)
 
     args = TrainingArguments(
-        "pretained_mnli",
+        "pretrained_mnli",
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         per_device_train_batch_size=16,
@@ -50,11 +49,11 @@ def main():
 
     trainer.train()
     trainer.evaluate()
-    trainer.save_model("pretained_mnli/best_model")
-    tokenizer.save_pretrained("pretained_mnli/best_model")
+    trainer.save_model("pretrained_mnli/best_model")
+    tokenizer.save_pretrained("pretrained_mnli/best_model")
 
 
-    model_checkpoint = f"pretained_mnli/best_model"
+    model_checkpoint = f"pretrained_mnli/best_model"
     rte_dataset = load_dataset("glue", "rte")
     metric_name = "accuracy"
     metric = load_metric('glue', "rte")
@@ -68,12 +67,12 @@ def main():
     model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=2)
 
     def preprocess_function(examples):
-        return tokenizer(examples["sentence2"], examples["sentence1"], truncation=True)
+        return tokenizer(examples["sentence1"], examples["sentence2"], truncation=True)
 
     encoded_dataset = rte_dataset.map(preprocess_function, batched=True)
 
     args = TrainingArguments(
-        "pretained_mnli_rte",
+        "pretrained_mnli_rte",
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         per_device_train_batch_size=16,
@@ -95,11 +94,10 @@ def main():
 
     trainer.train()
     trainer.evaluate()
-    trainer.save_model("pretained_mnli_rte/best_model")
-    tokenizer.save_pretrained("pretained_mnli_rte/best_model")
-    """
+    trainer.save_model("pretrained_mnli_rte/best_model")
+    tokenizer.save_pretrained("pretrained_mnli_rte/best_model")
 
-    model_checkpoint = f"pretained_mnli_rte/best_model"
+    model_checkpoint = f"pretrained_mnli_rte/best_model"
     fever_dataset = load_dataset("json", data_files={"train": f"{flair.cache_root}/datasets/fever/train.jsonl",
                                                "test": f"{flair.cache_root}/datasets/fever/test.jsonl",
                                                "dev": f"{flair.cache_root}/datasets/fever/dev.jsonl"})
@@ -117,12 +115,12 @@ def main():
     def preprocess_function(examples):
         labels = [1 if x == "SUPPORTS" else 0 for x in examples["label"]]
         examples["label"] = labels
-        return tokenizer(examples["context"], examples["query"], truncation=True)
+        return tokenizer(examples["query"], examples["context"], truncation=True)
 
     encoded_dataset = fever_dataset.map(preprocess_function, batched=True)
 
     args = TrainingArguments(
-        "pretained_mnli_rte_fever",
+        "pretrained_mnli_rte_fever",
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         per_device_train_batch_size=16,
@@ -144,8 +142,8 @@ def main():
 
     trainer.train()
     trainer.evaluate()
-    trainer.save_model("pretained_mnli_rte_fever/best_model")
-    tokenizer.save_pretrained("pretained_mnli_rte_fever/best_model")
+    trainer.save_model("pretrained_mnli_rte_fever/best_model")
+    tokenizer.save_pretrained("pretrained_mnli_rte_fever/best_model")
 
 if __name__ == "__main__":
     main()
