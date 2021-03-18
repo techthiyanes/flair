3e535d4bab38b10492aa4fbf5ee5790cb5e1a3ff
whoisjones <jonas.golde@gmail.com>
2021-03-18 23:59:56 +0100

    fix typo

--- MULTITASK.py
+++ MULTITASK.py
@@ -5,72 +5,81 @@ from flair.tokenization import SegtokTokenizer
 from flair.trainers import ModelTrainer
 from flair.datasets import TREC_50, CSVClassificationCorpus, SentenceDataset
 
+def make_text(data_point, text_columns):
+    return [data_point[0], " ".join(data_point[text_column] for text_column in text_columns)]
+
+def make_sentence(data_point, tokenizer):
+    s = Sentence(data_point[1], use_tokenizer=tokenizer)
+    s.add_label("class", data_point[0])
+    return s
 
 def get_corpora():
     tokenizer = SegtokTokenizer()
 
     # TREC50 CORPUS
-    trec50_label_name_map = {'ENTY:sport': 'question about entity sport',
-                             'ENTY:dismed': 'question about entity diseases medicine',
-                             'LOC:city': 'question about location city',
-                             'DESC:reason': 'question about description reasons',
-                             'NUM:other': 'question about number other',
-                             'LOC:state': 'question about location state',
-                             'NUM:speed': 'question about number speed',
-                             'NUM:ord': 'question about number order ranks',
-                             'ENTY:event': 'question about entity event',
-                             'ENTY:substance': 'question about entity element substance',
-                             'NUM:perc': 'question about number percentage fractions',
-                             'ENTY:product': 'question about entity product',
-                             'ENTY:animal': 'question about entity animal',
-                             'DESC:manner': 'question about description manner of action',
-                             'ENTY:cremat': 'question about entity creative pieces inventions books',
-                             'ENTY:color': 'question about entity color',
-                             'ENTY:techmeth': 'question about entity technique method',
-                             'NUM:dist': 'question about number distance measure',
-                             'NUM:weight': 'question about number weight',
-                             'LOC:mount': 'question about location mountains',
-                             'HUM:title': 'question about person title',
-                             'HUM:gr': 'question about person group organization of persons',
-                             'HUM:desc': 'question about person description',
-                             'ABBR:abb': 'question about abbreviation abbreviation',
-                             'ENTY:currency': 'question about entity currency',
-                             'DESC:def': 'question about description definition',
-                             'NUM:code': 'question about number code',
-                             'LOC:other': 'question about location other',
-                             'ENTY:other': 'question about entity other',
-                             'ENTY:body': 'question about entity body organ',
-                             'ENTY:instru': 'question about entity musical instrument',
-                             'ENTY:termeq': 'question about entity term equivalent',
-                             'NUM:money': 'question about number money prices',
-                             'NUM:temp': 'question about number temperature',
-                             'LOC:country': 'question about location country',
-                             'ABBR:exp': 'question about abbreviation expression',
-                             'ENTY:symbol': 'question about entity symbol signs',
-                             'ENTY:religion': 'question about entity religion',
-                             'HUM:ind': 'question about person individual',
-                             'ENTY:letter': 'question about entity letters characters',
-                             'NUM:date': 'question about number date',
-                             'ENTY:lang': 'question about entity language',
-                             'ENTY:veh': 'question about entity vehicle',
-                             'NUM:count': 'question about number count',
-                             'ENTY:word': 'question about entity word special property',
-                             'NUM:period': 'question about number period lasting time',
-                             'ENTY:plant': 'question about entity plant',
-                             'ENTY:food': 'question about entity food',
-                             'NUM:volsize': 'question about number volume size',
-                             'DESC:desc': 'question about description description'
-                             }
+    trec50_label_name_map = {
+        'ENTY:sport': 'question about entity sport',
+         'ENTY:dismed': 'question about entity diseases medicine',
+         'LOC:city': 'question about location city',
+         'DESC:reason': 'question about description reasons',
+         'NUM:other': 'question about number other',
+         'LOC:state': 'question about location state',
+         'NUM:speed': 'question about number speed',
+         'NUM:ord': 'question about number order ranks',
+         'ENTY:event': 'question about entity event',
+         'ENTY:substance': 'question about entity element substance',
+         'NUM:perc': 'question about number percentage fractions',
+         'ENTY:product': 'question about entity product',
+         'ENTY:animal': 'question about entity animal',
+         'DESC:manner': 'question about description manner of action',
+         'ENTY:cremat': 'question about entity creative pieces inventions books',
+         'ENTY:color': 'question about entity color',
+         'ENTY:techmeth': 'question about entity technique method',
+         'NUM:dist': 'question about number distance measure',
+         'NUM:weight': 'question about number weight',
+         'LOC:mount': 'question about location mountains',
+         'HUM:title': 'question about person title',
+         'HUM:gr': 'question about person group organization of persons',
+         'HUM:desc': 'question about person description',
+         'ABBR:abb': 'question about abbreviation abbreviation',
+         'ENTY:currency': 'question about entity currency',
+         'DESC:def': 'question about description definition',
+         'NUM:code': 'question about number code',
+         'LOC:other': 'question about location other',
+         'ENTY:other': 'question about entity other',
+         'ENTY:body': 'question about entity body organ',
+         'ENTY:instru': 'question about entity musical instrument',
+         'ENTY:termeq': 'question about entity term equivalent',
+         'NUM:money': 'question about number money prices',
+         'NUM:temp': 'question about number temperature',
+         'LOC:country': 'question about location country',
+         'ABBR:exp': 'question about abbreviation expression',
+         'ENTY:symbol': 'question about entity symbol signs',
+         'ENTY:religion': 'question about entity religion',
+         'HUM:ind': 'question about person individual',
+         'ENTY:letter': 'question about entity letters characters',
+         'NUM:date': 'question about number date',
+         'ENTY:lang': 'question about entity language',
+         'ENTY:veh': 'question about entity vehicle',
+         'NUM:count': 'question about number count',
+         'ENTY:word': 'question about entity word special property',
+         'NUM:period': 'question about number period lasting time',
+         'ENTY:plant': 'question about entity plant',
+         'ENTY:food': 'question about entity food',
+         'NUM:volsize': 'question about number volume size',
+         'DESC:desc': 'question about description description'
+        }
     trec_full: Corpus = TREC_50(label_name_map=trec50_label_name_map)
     trec_train = Corpus(train=trec_full.train, dev=trec_full.dev)
     trec_test = trec_full.test
 
     # AGNEWS CORPUS
-    agnews_label_name_map = {'1': 'World',
-                              '2': 'Sports',
-                              '3': 'Business',
-                              '4': 'Science Technology'
-                              }
+    agnews_label_name_map = {
+        '1': 'World',
+          '2': 'Sports',
+          '3': 'Business',
+          '4': 'Science Technology'
+    }
     column_name_map = {0: "label", 1: "text", 2: "text"}
     corpus_path = f"{flair.cache_root}/datasets/ag_news_csv"
     agnews_full: Corpus = CSVClassificationCorpus(
@@ -81,14 +90,9 @@ def get_corpora():
         label_name_map=agnews_label_name_map
     )
     agnews_train = Corpus(train=agnews_full.train, dev=agnews_full.dev)
-    test_split_sentences = []
-    for data_point in agnews_full.test.raw_data:
-        text = " ".join(
-            [data_point[text_column] for text_column in agnews_full.train.dataset.text_columns]
-        )
-        sentence = Sentence(text, use_tokenizer=tokenizer)
-        sentence.add_label("class", data_point[0])
-        test_split_sentences.append(sentence)
+    text_columns = agnews_full.train.dataset.text_columns
+    test_split_sentences = [make_text(data_point, text_columns) for data_point in agnews_full.test.raw_data]
+    test_split_sentences = [make_sentence(data_point, tokenizer) for data_point in test_split_sentences]
     agnews_test = SentenceDataset(test_split_sentences)
 
     # DBPEDIA CORPUS
@@ -107,7 +111,7 @@ def get_corpora():
                               '13': 'Film',
                               '14': 'Written Work'
                               }
-    column_name_map = {0: "label", 2: "text"}
+    column_name_map = {0: "label", 1: "text", 2: "text"}
     corpus_path = f"{flair.cache_root}/datasets/dbpedia_csv"
     dbpedia_full: Corpus = CSVClassificationCorpus(
         corpus_path,
@@ -117,14 +121,9 @@ def get_corpora():
         label_name_map=dbpedia_label_name_map
     )
     dbpedia_train = Corpus(train=dbpedia_full.train, dev=dbpedia_full.dev).downsample(0.25)
-    test_split_sentences = []
-    for data_point in dbpedia_full.test.raw_data:
-        text = " ".join(
-            [data_point[text_column] for text_column in dbpedia_full.train.dataset.text_columns]
-        )
-        sentence = Sentence(text, use_tokenizer=tokenizer)
-        sentence.add_label("class", data_point[0])
-        test_split_sentences.append(sentence)
+    text_columns = dbpedia_full.train.dataset.text_columns
+    test_split_sentences = [make_text(data_point, text_columns) for data_point in dbpedia_full.test.raw_data]
+    test_split_sentences = [make_sentence(data_point, tokenizer) for data_point in test_split_sentences]
     dbpedia_test = SentenceDataset(test_split_sentences)
 
     # AMAZON CORPUS
@@ -143,15 +142,10 @@ def get_corpora():
         delimiter=',',
         label_name_map=amazon_label_name_map
     )
-    amazon_train = Corpus(train=amazon_full.train, dev=amazon_full.dev).downsample(0.05)
-    test_split_sentences = []
-    for data_point in amazon_full.test.raw_data:
-        text = " ".join(
-            [data_point[text_column] for text_column in amazon_full.train.dataset.text_columns]
-        )
-        sentence = Sentence(text, use_tokenizer=tokenizer)
-        sentence.add_label("class", data_point[0])
-        test_split_sentences.append(sentence)
+    amazon_train = Corpus(train=amazon_full.train, dev=amazon_full.dev).downsample(0.03)
+    text_columns = amazon_full.train.dataset.text_columns
+    test_split_sentences = [make_text(data_point, text_columns) for data_point in amazon_full.test.raw_data]
+    test_split_sentences = [make_sentence(data_point, tokenizer) for data_point in test_split_sentences]
     amazon_test = SentenceDataset(test_split_sentences)
 
     # YELP CORPUS
@@ -170,16 +164,22 @@ def get_corpora():
         delimiter=',',
         label_name_map=yelp_label_name_map
     )
-    yelp_train = Corpus(train=yelp_full.train, dev=yelp_full.dev).downsample(0.05)
-    test_split_sentences = []
-    for data_point in yelp_full.test.raw_data:
-        text = " ".join(
-            [data_point[text_column] for text_column in yelp_full.train.dataset.text_columns]
-        )
-        sentence = Sentence(text, use_tokenizer=tokenizer)
-        sentence.add_label("class", data_point[0])
-        test_split_sentences.append(sentence)
+    yelp_train = Corpus(train=yelp_full.train, dev=yelp_full.dev).downsample(0.1)
+    text_columns = yelp_full.train.dataset.text_columns
+    test_split_sentences = [make_text(data_point, text_columns) for data_point in yelp_full.test.raw_data]
+    test_split_sentences = [make_sentence(data_point, tokenizer) for data_point in test_split_sentences]
     yelp_test = SentenceDataset(test_split_sentences)
+
+    print(trec_train)
+    print(trec_test)
+    print(agnews_train)
+    print(len(agnews_test))
+    print(dbpedia_train)
+    print(len(dbpedia_test))
+    print(amazon_train)
+    print(len(amazon_test))
+    print(yelp_train)
+    print(len(yelp_test))
 
     return {"trec50":
                 {
@@ -284,12 +284,12 @@ if __name__ == "__main__":
         "bert-entailment-standard":
             {
                 "path": "2_entailment_standard",
-                "model": "distilbert_entailment/pretrained_mnli/best_model"
+                "model": "distilbert_entailment_label_sep_text/pretrained_mnli/best_model"
             },
         "bert-entailment-advanced":
             {
                 "path": "2_entailment_advanced",
-                "model": "distilbert_entailment/pretrained_mnli_rte_fever/best_model"
+                "model": "distilbert_entailment_label_sep_text/pretrained_mnli_rte_fever/best_model"
             }
     }
     corpora = get_corpora()
