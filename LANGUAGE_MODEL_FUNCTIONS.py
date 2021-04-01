7f4a358b09927198b14b4bcd026d0f20ff57d717
whoisjones <jonas.golde@gmail.com>
2021-04-01 23:16:06 +0200

    transformer test

--- LANGUAGE_MODEL_FUNCTIONS.py
+++ LANGUAGE_MODEL_FUNCTIONS.py
@@ -10,14 +10,13 @@ def get_model(model_checkpoint, num_labels):
     return model, tokenizer
 
 def get_model_with_new_classifier(model_checkpoint, num_labels):
-    encoder = BertForSequenceClassification.from_pretrained(model_checkpoint)
-    decoder = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=num_labels)
-    encoder = copy.deepcopy(encoder.bert)
-    dropout = copy.deepcopy(decoder.dropout)
-    decoder = copy.deepcopy(decoder.classifier)
-    new_model = NewModel(encoder, dropout, decoder)
+    old_model = BertForSequenceClassification.from_pretrained(model_checkpoint)
+    copied_model = copy.deepcopy(old_model.bert)
+    del old_model
+    copied_model.classifier = torch.nn.Linear(768, num_labels)
+    torch.nn.init.xavier_uniform_(copied_model.classifier.weight)
     tokenizer = BertTokenizer.from_pretrained(model_checkpoint, use_fast=True)
-    return new_model, tokenizer
+    return copied_model, tokenizer
 
 def read_csv(file, samples = None):
     texts = []
