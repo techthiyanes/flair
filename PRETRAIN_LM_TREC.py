53899d60ea2b0952c6e89af4e17072dac911dcb1
whoisjones <jonas.golde@gmail.com>
2021-04-05 21:40:11 +0200

    TRANSFER_ASPECT.py new try

--- PRETRAIN_LM_TREC.py
+++ PRETRAIN_LM_TREC.py
@@ -1,5 +1,5 @@
 from transformers import Trainer, TrainingArguments
-from LANGUAGE_MODEL_FUNCTIONS import get_model, read_trec, Dataset, get_model_with_new_classifier
+from LANGUAGE_MODEL_FUNCTIONS import get_model, read_trec, Dataset, get_model_with_new_classifier, get_bart_model_with_new_classifier
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 def train(model_checkpoint, train_texts, train_labels, test_texts, test_labels):
@@ -11,17 +11,19 @@ def train(model_checkpoint, train_texts, train_labels, test_texts, test_labels):
     elif model_checkpoint == 'entailment_label_sep_text/pretrained_mnli_rte_fever/best_model':
         mod = "mnli_adv"
     else:
-        mod = "unknown"
+        mod = "bart"
 
     if mod == "bert":
         model, tokenizer = get_model(model_checkpoint, num_labels)
+    elif mod == "bart":
+        model, tokenizer = get_bart_model_with_new_classifier(model_checkpoint, num_labels)
     else:
         model, tokenizer = get_model_with_new_classifier(model_checkpoint, num_labels)
 
-    train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=512)
+    train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=512, return_tensors="pt")
     train_dataset = Dataset(train_encodings, train_labels)
 
-    test_encodings = tokenizer(test_texts, truncation=True, padding=True, max_length=512)
+    test_encodings = tokenizer(test_texts, truncation=True, padding=True, max_length=512, return_tensors="pt")
     test_dataset = Dataset(test_encodings, test_labels)
 
     def compute_metrics(pred):
@@ -59,7 +61,7 @@ def train(model_checkpoint, train_texts, train_labels, test_texts, test_labels):
     tokenizer.save_pretrained(f"experiments_v2/0_bert_baseline/trec/finetuned/{mod}/best_model")
 
 if __name__ == "__main__":
-    model_checkpoints = ['bert-base-uncased','entailment_label_sep_text/pretrained_mnli/best_model', 'entailment_label_sep_text/pretrained_mnli_rte_fever/best_model']
+    model_checkpoints = ['facebook/bart-large-mnli']
     train_texts, test_texts, train_labels, test_labels, class_to_datapoint_mapping = read_trec(num_labels=6)
     for model_checkpoint in model_checkpoints:
         train(model_checkpoint, train_texts, train_labels, test_texts, test_labels)
