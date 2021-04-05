544acfb6b994c1b05b62c5b5f7c35432ade6c04a
whoisjones <jonas.golde@gmail.com>
2021-04-05 10:43:54 +0200

    multitask trec

--- LANGUAGE_MODEL_YELP.py
+++ LANGUAGE_MODEL_YELP.py
@@ -65,7 +65,7 @@ def train(model_checkpoint, run, samples, train_texts, train_labels, test_texts,
             f.write(f"{metric}: {score} \n")
 
 if __name__ == "__main__":
-    model_checkpoints = ['experiments_v2/0_bert_baseline/yelp/finetuned/bert/best_model', 'experiments_v2/0_bert_baseline/yelp/finetuned/mnli_base/best_model', 'experiments_v2/0_bert_baseline/yelp/finetuned/mnli_adv/best_model']
+    model_checkpoints = ['experiments_v2/0_bert_baseline/yelp/finetuned/mnli_base/best_model', 'experiments_v2/0_bert_baseline/yelp/finetuned/mnli_adv/best_model']
     number_data_points = [1,2,4,8,10,100]
     runs = [1,2,3,4,5]
     train_texts, train_labels, class_to_datapoint_mapping = read_csv('../.flair/datasets/yelp_review_full_csv/train.csv')
