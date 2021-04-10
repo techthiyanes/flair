8c2b640886fedd8e046b59348d9f2f03fce5fcf6
whoisjones <jonas.golde@gmail.com>
2021-04-10 17:36:07 +0200

    transfer aspect

--- TESTMT.py
+++ TESTMT.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 
-from flair.data import Sentence, Corpus
+from LANGUAGE_MODEL_FUNCTIONS import read_csv, sample_datasets
+from flair.data import Sentence, Corpus, TARSCorpus
+from flair.models.multitask_model.task_model import RefactoredTARSClassifier
 from flair.models.tars_tagger_model import TARSTagger
 from flair.datasets import CONLL_03
 from flair.trainers import ModelTrainer
@@ -36,14 +38,41 @@ def extract_XML(path):
     return data
 
 def main():
+    """
     laptop_data = extract_XML('aspect_data/Laptop_Train_v2.xml')
 
     laptop_corpus = Corpus(laptop_data)
     tagger = TARSTagger("conll_ner", laptop_corpus.make_tag_dictionary("polarity"), tag_type="polarity")
+    """
+    label_name_map = {'1':'very negative restaurant sentiment',
+                      '2':'negative restaurant sentiment',
+                      '3':'neutral restaurant sentiment',
+                      '4':'positive restaurant sentiment',
+                      '5':'very positive restaurant sentiment'
+                      }
+    train_texts, train_labels, class_to_datapoint_mapping = read_csv(f"{flair.cache_root}/datasets/yelp_review_full_csv/train.csv")
+    train_texts, train_labels = sample_datasets(original_texts=train_texts,
+                                                original_labels=train_labels,
+                                                number_of_samples=600,
+                                                class_to_datapoint_mapping=class_to_datapoint_mapping)
+    train_labels = [x+1 for x in train_labels]
+    sentences = []
+    for text, label in zip(train_texts, train_labels):
+        sentence = Sentence(text)
+        sentence.add_label("class", label_name_map[str(label)])
+        sentences.append(sentence)
 
-    trainer = ModelTrainer(tagger, laptop_corpus)
+    yelp_corpus = Corpus(sentences)
 
-    trainer.train(base_path="testy",  # path to store the model artifacts
+    tars_corpus = TARSCorpus(
+        {"corpus": yelp_corpus, "task_name": "food_review"},
+    )
+
+    tars_classifier = RefactoredTARSClassifier(tars_corpus.tasks)
+
+    trainer = ModelTrainer(tars_classifier, tars_corpus)
+
+    trainer.train(base_path="testy2",  # path to store the model artifacts
                   learning_rate=0.02,  # use very small learning rate
                   mini_batch_size=16,
                   max_epochs=20,
@@ -51,5 +80,5 @@ def main():
 
 if __name__ == "__main__":
     import flair
-    flair.device = "cuda:0"
+    flair.device = "cuda:1"
     main()
